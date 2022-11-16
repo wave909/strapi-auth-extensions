0907ad50f34a4a55803ca4a4a704e2c8249eaf80
kiri <kirillknizhov@mail.ru>
2022-11-16 15:54:57 +0700

    sanitizing user output

--- packages/strapi-plugin-auth-ext/server/services/transforms.js
+++ packages/strapi-plugin-auth-ext/server/services/transforms.js
@@ -1,17 +1,19 @@
 const emailRegExp =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const { sanitize } = require("@strapi/utils");
 
 module.exports = ({ strapi }) => ({
   async formatAuthStepOutput({ step, user, query }) {
     console.log({ user, step });
     if (Number(step) + 1 >= (user.auth_steps_number || 1)) {
+      const userEntity = await strapi.entityService.findOne(
+        "plugin::users-permissions.user",
+        user.id,
+        { populate: query.populate || [] }
+      );
+      const userSchema = strapi.getModel("plugin::users-permissions.user");
       return {
-        //TODO sanitize user
-        user: await strapi.entityService.findOne(
-          "plugin::users-permissions.user",
-          user.id,
-          { populate: query.populate || [] }
-        ),
+        user: await sanitize.contentAPI.output(userEntity, userSchema),
         token: await strapi
           .service("plugin::users-permissions.jwt")
           .issue({ id: user.id }),
